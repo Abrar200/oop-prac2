5c5c248223203dde792fc434ba83fa8c674d6a4e
Abrar200 <59603112+Abrar200@users.noreply.github.com>
2024-08-01 22:55:29 +0930

    Update function-1-2.cpp

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -1,17 +1,19 @@
-// function-1-2.cpp
 int is_identity(int array[10][10]) {
-    for (int i = 0; i < 10; ++i) {
-        for (int j = 0; j < 10; ++j) {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
             if (i == j) {
+                // Check if diagonal elements are 1
                 if (array[i][j] != 1) {
-                    return 0; // Diagonal element is not 1
+                    return 0;
                 }
             } else {
+                // Check if non-diagonal elements are 0
                 if (array[i][j] != 0) {
-                    return 0; // Non-diagonal element is not 0
+                    return 0;
                 }
             }
         }
     }
-    return 1; // It is an identity matrix
+    // If we've made it through all checks, it's an identity matrix
+    return 1;
 }
