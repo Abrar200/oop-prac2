d106c8adf30094a1b74c98b7393be5db5cb21a17
Abrar200 <59603112+Abrar200@users.noreply.github.com>
2024-08-01 22:59:15 +0930

    Update function-1-5.cpp

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -1,14 +1,17 @@
-// function-1-5.cpp
 #include <iostream>
 
 void print_summed(int array1[3][3], int array2[3][3]) {
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 3; ++j) {
-            std::cout << (array1[i][j] + array2[i][j]);
-            if (j < 3 - 1) {
-                std::cout << " "; // Print space between elements in a row
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            int sum = array1[i][j] + array2[i][j];
+            std::cout << sum;
+            
+            // Add a space after each element, except for the last one in a row
+            if (j < 2) {
+                std::cout << " ";
             }
         }
-        std::cout << std::endl; // Print newline after each row
+        // Add a newline after each row
+        std::cout << std::endl;
     }
 }
