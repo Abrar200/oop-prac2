a3eef8256ec36e431b2daee34734921e3272e148
Abrar200 <59603112+Abrar200@users.noreply.github.com>
2024-08-01 22:58:21 +0930

    Update main-1-4.cpp

--- main-1-4.cpp
+++ main-1-4.cpp
@@ -1,15 +1,16 @@
-// main-1-4.cpp
 #include <iostream>
-#include "function-1-4.cpp" // Include the source file containing the print_scaled function
+
+// Function declaration
+void print_scaled(int array[3][3], int scale);
 
 int main() {
     int scale = 3;
-    int threebythree[3][3] = {
-        {0, 1, 2},
-        {3, 4, 5},
-        {6, 7, 8}
-    };
+    int threebythree[3][3] = {{0,1,2},{3,4,5},{6,7,8}};
 
+    std::cout << "Original matrix:" << std::endl;
+    print_scaled(threebythree, 1);  // Print original matrix (scale = 1)
+
+    std::cout << "\nScaled matrix (scale = " << scale << "):" << std::endl;
     print_scaled(threebythree, scale);
 
     return 0;
