d9059c210464c5235640cef0dbe740d9e3ff8504
Abrar200 <59603112+Abrar200@users.noreply.github.com>
2024-08-01 23:28:52 +0930

    Update function-2-3.cpp

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -1,36 +1,30 @@
-// function-2-3.cpp
-#include <iostream>
-
-// Function to check if an array is a palindrome
 bool is_palindrome(int integers[], int length) {
-    if (length <= 0) return false;  // Return false if the length is 0 or negative
-
-    for (int i = 0; i < length / 2; ++i) {
+    if (length <= 0) return false;
+    
+    for (int i = 0; i < length / 2; i++) {
         if (integers[i] != integers[length - 1 - i]) {
-            return false;  // If elements don't match, it's not a palindrome
+            return false;
         }
     }
-    return true;  // All matched, it is a palindrome
+    return true;
 }
 
-// Function to sum elements of an array
 int sum_array_elements(int integers[], int length) {
-    if (length <= 0) return -1;  // Return -1 if the length is 0 or negative
-
+    if (length <= 0) return -1;
+    
     int sum = 0;
-    for (int i = 0; i < length; ++i) {
+    for (int i = 0; i < length; i++) {
         sum += integers[i];
     }
     return sum;
 }
 
-// Function to sum elements if the array is a palindrome
 int sum_if_palindrome(int integers[], int length) {
-    if (length <= 0) return -1;  // Return -1 if the length is 0 or negative
-
+    if (length <= 0) return -1;
+    
     if (is_palindrome(integers, length)) {
         return sum_array_elements(integers, length);
     } else {
-        return -2;  // Return -2 if the array is not a palindrome
+        return -2;
     }
 }
