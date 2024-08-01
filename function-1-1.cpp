8c969f87539573ef6fb9f38ab2765e7cbe95b439
Abrar200 <59603112+Abrar200@users.noreply.github.com>
2024-08-01 22:51:43 +0930

    Update function-1-1.cpp

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,7 +1,6 @@
-// function-1-1.cpp
 int sum_diagonal(int array[4][4]) {
     int sum = 0;
-    for (int i = 0; i < 4; ++i) {
+    for (int i = 0; i < 4; i++) {
         sum += array[i][i];
     }
     return sum;
