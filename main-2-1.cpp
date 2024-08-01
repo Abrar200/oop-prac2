d5f842c96e98351dcef9969cbfcb01a17e96fff4
Abrar200 <59603112+Abrar200@users.noreply.github.com>
2024-08-01 23:04:37 +0930

    Update main-2-1.cpp

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -1,11 +1,10 @@
-// main-2-1.cpp
-#include <iostream>
-#include "function-2-1.cpp" // Include the source file containing the print_binary_str function
+#include "function-2-1.cpp"
 
 int main() {
+    // Test cases
     print_binary_str("0");
     print_binary_str("789");
     print_binary_str("123456789");
-
+    
     return 0;
 }
