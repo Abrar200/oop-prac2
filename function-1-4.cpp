54e92a0c459498e8d0ea0094a2ab0db289c303f7
Abrar200 <59603112+Abrar200@users.noreply.github.com>
2024-08-01 22:58:06 +0930

    Update function-1-4.cpp

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -1,14 +1,16 @@
-// function-1-4.cpp
 #include <iostream>
 
 void print_scaled(int array[3][3], int scale) {
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 3; ++j) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
             std::cout << array[i][j] * scale;
-            if (j < 3 - 1) {
-                std::cout << " "; // Print space between elements in a row
+            
+            // Add a space after each element, except for the last one in a row
+            if (j < 2) {
+                std::cout << " ";
             }
         }
-        std::cout << std::endl; // Print newline after each row
+        // Add a newline after each row
+        std::cout << std::endl;
     }
 }
